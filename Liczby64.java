ac8dd49b1272e40d8557098d916ed599cc061d36
btertil <bondiev@gmail.com>
2018-02-04 13:42:25 +0100

    Create Liczby64.java

---
+++ Liczby64.java
@@ -0,0 +1,65 @@
+public class Liczby64 {
+
+
+
+	public static void main(String[] args)
+
+	{
+
+	   int prz, i, j, zakres;
+
+	   int znalezione, reszta, flaga;
+
+	   prz = 512000;
+
+
+
+	   znalezione = 1; //2 to liczba pierwsza (1 NIE jest liczbą pierwszą bo pierwsze muszą by wieksze od 1)
+
+
+
+
+
+	   for (i=3; i <= prz; i++) {
+
+		flaga = 0;
+
+		zakres = (int) (i/2)+1;
+
+		for (j=2; j<zakres; j++) {
+
+		    reszta = i%j;
+
+		    if (reszta == 0) {
+
+			flaga++;
+
+			break;
+
+		    }
+
+		}
+
+
+
+		if (flaga == 0) {
+
+		    znalezione++;
+
+		}
+
+
+
+	   }
+
+
+
+	    System.out.printf("W przedziale 0 - %d naleziono: %d liczb lierwszych\n", prz, znalezione);
+
+
+
+	}
+
+
+
+}
