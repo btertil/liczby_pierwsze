012c5bd8d10a5c08f03aae9ae28760f5ec24a7e9
btertil <bondiev@gmail.com>
2018-02-04 13:33:27 +0100

    Create liczby_benchmark.sh

---
+++ liczby_benchmark.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+echo
+echo
+echo ++++++++++++++++++++
+echo
+echo Program porównuje prędkość liczenia liczb pierwszych
+echo implementujących tą samą metodę w różnych językach
+echo
+echo ++++++++++++++++++++
+echo
+echo
+echo mały przedział 30k
+echo
+echo "Assembly (NASM):"
+time ./liczbyn
+echo
+echo C:
+time ./liczbyc
+echo
+echo Java:
+time java Liczby
+echo
+echo Scala:
+time scala liczbys
+echo
+echo ++++++++++++++++++++
+echo teraz duży przedział 512k
+echo
+echo C:
+time ./liczbyc64
+echo
+echo Java:
+time java Liczby64
+echo
+echo Scala:
+time scala liczbys64
+echo
