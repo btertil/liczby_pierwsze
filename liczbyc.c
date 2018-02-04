44e56c1485436276428a4cdce7c239aecbdcef0f
btertil <bondiev@gmail.com>
2018-02-04 13:09:19 +0100

    Create liczbyc.c

---
+++ liczbyc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+   unsigned int prz, i, j, zakres;
+   unsigned int znalezione, reszta, flaga;
+   prz = 30000;
+
+   znalezione = 1; //2 to liczba pierwsza (1 NIE jest liczbą pierwszą bo pierwsze muszą by wieksze od 1)
+
+
+   for (i=3; i <= prz; i++) {
+        flaga = 0;
+		zakres = (unsigned int) (i/2)+1;
+        for (j=2; j<zakres; j++) {
+            reszta = i%j;
+            if (reszta == 0) {
+                flaga++;
+                break;
+            }
+        }
+
+        if (flaga == 0) {
+            znalezione++;
+        }
+
+   }
+
+    printf("W przedziale 0 - %i naleziono: %i liczb lierwszych\n", prz, znalezione);
+
+    return 0;
+}
