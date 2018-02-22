14d08aed91aa4c11d71582e84da1a12bf5e8a2fe
bartek <bondiev@gmail.com>
2018-02-22 04:11:29 +0100

    poprawki w skryptach compile*.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+gcc -o liczbycp liczbycp.c -lm
 gcc -o liczbyc liczbyc.c -lm
 gcc -o liczbyc64 liczbyc64.c -lm
 gcc -o primes primes.c -lm
 
+javac Liczbyp.java
 javac Liczby.java
 javac Liczby64.java
 
+scalac Liczbysp.scala
 scalac Liczbys.scala
 scalac Liczbys64.scala
 
