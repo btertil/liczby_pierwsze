c820288ede82c43a199d5c9bc29d7404002eee03
bartek <bondiev@gmail.com>
2018-02-23 18:15:59 +0100

    dodany dodatkowy benchmark2 z parametrami

---
+++ benchmark2.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+clear
+echo
+echo
+echo ++++++++++++++++++++
+echo
+echo Program porównuje prędkość liczenia liczb pierwszych
+echo implementujących tą samą metodę w różnych językach
+echo
+echo ++++++++++++++++++++
+echo
+echo
+for i in 100000 1000000 10000000 100000000
+do
+echo
+echo
+echo test dla przedziału 0 - $i
+echo ++++++++++++++++++++
+echo
+echo C:
+time ./liczbycp $i
+echo
+echo Java:
+time java Liczbyp $i
+echo
+echo Scala:
+time scala Liczbysp $i
+if [ $i -lt  10000000 ] ; then
+	echo
+	echo
+	echo PHP:
+	time php liczbyp.php $i
+	echo
+	echo Python:
+	time python liczbyp.py $i
+else
+	echo
+	echo
+	echo "Testy dla PHP i Pythona pominięto z uwagi na zbyt duży przedział (powyżej 1000000)"
+fi
+echo
+echo ++++++++++++++++++++
+echo
+echo
+
+done
+  
+echo "done!"
+
+
